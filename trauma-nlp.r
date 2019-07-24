d694a5bec7cb60977c08042e6721c99a25cb873a
Elizabeth Beam <ehbeam@Elizabeths-MacBook-Air.local>
2019-07-24 09:58:05 -0700

    Function for basic preprocessing

--- trauma-nlp.r
+++ trauma-nlp.r
@@ -1,6 +1,5 @@
-# Install and import NLP library
-install.packages("textstem")
-library(textstem)
+# Load the NLP library
+require("textstem")
 
 # Specify term lists for trauma types
 type_names = list("MILITARY", "MURDER", "DEATH")
@@ -9,63 +8,61 @@ types = list(list("war", "battle", "gun", "grenade", "military"),
              list("grave", "funeral", "death", "die", "on edge"))
 n = length(types)
 
-# Preprocess term lists
-types_proc = list()
+# Function to preprocess a list of words
+preprocess = function(words) {
+  words = tolower(words) # Convert to lowercase
+  words = gsub("\r?\n|\r|\t", " ", words) # Remove line breaks
+  words = gsub("[[:punct:] ]+", " ", words) # Remove punctuation
+  words = stem_words(as.vector(words)) # Perform stemming
+  return(words)
+}
+
+# Preprocess trauma type lists
+proc_types = list()
 for (i in 1:n) {
-  for (j in 1:length(types[[i]])) {
-    words = types[[i]]
-    words = stem_strings(words)
-    words = gsub(" ", "_", words)
-    types_proc[[i]] = words
-  }
+  words = preprocess(types[[i]])
+  words = gsub(" ", "_", words) # Combine phrases into terms
+  proc_types[[i]] = words
 }
 
-# Load inputs from a directory with one text file per patient
-setwd(getwd())
-files = list.files("texts")
-m = length(files)
-texts = c()
-for (i in 1:m) {
-  file = paste("texts/", files[i], sep="")
-  text = readChar(file, file.info(file_name)$size)
-  texts = c(texts, text)
-}
-input_names = gsub(".txt", "", files)
-
-# Extract phrases to combine into terms
+# Extract phrases from trauma type lists
 phrases = c()
 for (i in 1:n) { 
   for (j in 1:length(types[[i]])) {
-    if (grepl("_", types[[i]][[j]])) {
-      phrases = c(phrases, types[[i]][[j]])
+    if (grepl("_", proc_types[[i]][[j]])) {
+      phrases = c(phrases, proc_types[[i]][[j]])
     }
   } 
 }
 
-# Perform basic text preprocessing
-proc = list()
-for (i in 1:n) {
-  text = tolower(texts[i]) # Convert to lowercase
-  text = (gsub("\r?\n|\r|\t", " ", text)) # Remove line breaks
-  text = (gsub("[[:punct:] ]+", " ", text)) # Remove punctuation
+# Load and preprocess input text files
+setwd(getwd())
+files = list.files("texts")
+input_names = gsub(".txt", "", files)
+m = length(files)
+proc_texts = list()
+for (i in 1:m) {
+  file = paste("texts/", files[i], sep="")
+  text = readChar(file, file.info(file_name)$size)
   words = strsplit(text, " ") # Split into a list of words
-  words = stem_words(as.vector(words[[1]]))# Perform stemming
-  for (phrase in phrases) {
+  words = preprocess(words[[1]]) # Preprocess the word list
+  text = paste(words, collapse = " ") # Recombine into a string
+  for (phrase in phrases) { # Combine phrases from trauma types
     phrase_with_spaces = gsub("_", " ", phrase)
-    words = gsub(phrase_with_spaces, phrase, words)
-  } 
-  proc[[i]] = words
+    text = gsub(phrase_with_spaces, phrase, text)
+  }
+  proc_texts[i] = strsplit(text, " ") # Split into a term list
 }
 
-# Score trauma types in each text
+# Score texts by the proportion of terms that occurred from each trauma type
 scores = matrix(data=NA, nrow=n, ncol=length(types), 
                 dimnames=list(input_names, type_names))
 for (i in 1:m) {
-  input_words = proc[[i]]
+  input_words = proc_texts[[i]]
   for (j in 1:n) {
-    type_words = types[[j]]
+    type_words = proc_types[[j]]
     intersection = intersect(input_words, type_words)
-    scores[i,j] = length(intersection) / length(type_words)
+    scores[i,j] = length(intersection) / length(type_words) 
   }
 }
 write.csv(scores, file="scores.csv")
